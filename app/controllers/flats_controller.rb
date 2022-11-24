1f0a44045b9e0c1cadc409d238ad467c67403e00
Shawn McSparron <shawnmc_28@hotmail.com>
2022-11-24 20:22:20 +1100

    new form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,4 +4,24 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to root_path, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
 end
